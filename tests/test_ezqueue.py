7457f59c829be26d13c292d3a42b41fc5fec93db
jwcnewton <jwcnewton@gmail.com>
2021-07-16 22:27:17 +0100

    Add unit tests and update version

--- tests/test_ezqueue.py
+++ tests/test_ezqueue.py
@@ -1,10 +1,29 @@
 from src.ezqueue.ezqueue import ezqueue
 
 
+def test_smoke():
+    queue = ezqueue(2)
+    expectedQueue = [{"_id": 1, "data": "2"}]
+    queue.put(1, "1")
+    queue.put(1, "2")
+
+    assert list(queue.getQueue()) == expectedQueue
+
+
 def test_put_unique_key():
     queue = ezqueue(4)
-
+    expectedQueue = [{"_id": 1, "data": "2"}]
     queue.put(1, "1")
     queue.put(1, "2")
 
     assert len(queue.getQueue()) != 2
+    assert list(queue.getQueue()) == expectedQueue
+
+
+def test_get_by_id_returns_expected_result():
+    queue = ezqueue(2)
+
+    queue.put(1, "1")
+    queue.put(2, "2")
+
+    assert queue.getById(2)["data"] == "2"
